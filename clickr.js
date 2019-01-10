73c8b981e02de690302bc660b5c93e642974331b
Memento-mo <gpologov@gmail.com>
2019-01-10 16:38:45 +0300

    Create app

---
+++ clickr.js
@@ -0,0 +1,34 @@
+let clicks = 1;
+const TIMEOUT = 5000;
+
+const button = document.querySelector('#button')
+const display = document.querySelector('#display')
+const counter = document.querySelector('#counter')
+
+button.onclick = start;
+
+function start() {
+    const startTime = Date.now()
+
+    display.textContent = formatTime(TIMEOUT);
+    button.onclick = () => counter.textContent = clicks++
+
+    const interval = setInterval(() => {
+        const delta = Date.now() - startTime;
+        display.textContent = formatTime(TIMEOUT - delta);
+
+    }, 100)
+
+    const timeout = setTimeout(() => {
+        button.onclick = null;
+        display.textContent = 'Game Over'
+
+        clearInterval(interval)
+        clearTimeout(timeout);
+    }, TIMEOUT)
+
+}
+
+function formatTime(ms) {
+    return Number.parseFloat(ms / 1000).toFixed(2);
+}
